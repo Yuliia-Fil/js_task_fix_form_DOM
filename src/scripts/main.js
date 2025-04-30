dea65bed76b84149daca334a82bb45edc32ecd22
Yulia Fil <filyulia.yf@gmail.com>
2025-04-30 21:12:38 +0300

    added check for id and name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,19 @@
 
 const inputs = [...document.querySelectorAll('input')];
 
+
+
 for (const input of inputs) {
   const label = document.createElement('label');
+  
+  if (input.hasAttribute('id')) {
+    input.before(label);
+    label.setAttribute('for', `${input.id}`);
+    label.classList.add('field-label');
+  }
 
-  input.before(label);
-  label.setAttribute('for', `${input.id}`);
-  label.classList.add('field-label');
+  if (input.hasAttribute('name')) {
   label.textContent = input.name;
   input.setAttribute('placeholder', `${input.name.toUpperCase()}`);
+}
 }
