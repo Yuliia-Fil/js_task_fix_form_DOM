51d6786abbec1f9a2d0541758bb2a87c2058f7fd
Yulia Fil <filyulia.yf@gmail.com>
2025-04-30 21:13:05 +0300

    make build

---
+++ js_task_fix_form_DOM.f75de5e1.js
@@ -0,0 +1,18 @@
+'use strict';
+const inputs = [
+    ...document.querySelectorAll('input')
+];
+for (const input of inputs){
+    const label = document.createElement('label');
+    if (input.hasAttribute('id')) {
+        input.before(label);
+        label.setAttribute('for', `${input.id}`);
+        label.classList.add('field-label');
+    }
+    if (input.hasAttribute('name')) {
+        label.textContent = input.name;
+        input.setAttribute('placeholder', `${input.name.toUpperCase()}`);
+    }
+}
+
+//# sourceMappingURL=js_task_fix_form_DOM.f75de5e1.js.map
